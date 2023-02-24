fa18155e2e4897136abadf29b2cd779c169c7fb1
Brighton Joshua <122241207+Brighton-Joshua@users.noreply.github.com>
2023-02-24 22:28:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validate() {
     const inputval = document.getElementById("tel").value;
 
 
-    let Airtel = ["0802", "0808", "0812", "0701", "0708", "0901", "0902", "0907"];
+    let Airtel = ["0802", "0808", "0812", "0701", "0708", "0901", "0902", "0907", "0912"];
     let MTN = ["0803", "0806", "0814", "0810", "0813", "0816", "0703", "0706", "0903", "0906"];
     let Glo = ["0805", "0807", "0811", "0815", "0705", "0905"];
     let mobile = ["0809", "0817", "0818", "0908", "0909"];
@@ -30,6 +30,9 @@ function validate() {
         document.getElementById("airtel-alert").style.visibility = "visible";
     }
     else if (inputval === Airtel[7]) {
+        document.getElementById("airtel-alert").style.visibility = "visible";
+    }
+    else if (inputval === Airtel[8]) {
         document.getElementById("airtel-alert").style.visibility = "visible";
     }
     
